a6994048bfe501a8a45c735b82ca21bc6bfe8e34
shivamdhammi <43827900+shivamdhammi@users.noreply.github.com>
2018-10-10 12:08:25 +0530

    Update SProfileEdit.java

--- SProfileEdit.java
+++ SProfileEdit.java
@@ -12,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -70,8 +71,9 @@ public class SProfileEdit extends AppCompatActivity {
 
         UserName.setText(test.substring(0,test.indexOf('@')));
         test =UserName.getText().toString();
+        //Toast.makeText(getApplicationContext(),test,Toast.LENGTH_LONG).show();
 
-        myRef = FirebaseDatabase.getInstance().getReference("SSO/"+test);
+        myRef = FirebaseDatabase.getInstance().getReference("SSO");
 
         Save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +81,14 @@ public class SProfileEdit extends AppCompatActivity {
                 saveUserInfo();
                 //Store the image and display name in Firebase Storage.
                 //we use profileImageURL to store it to Storage.
+
+                //////
+
+                SSOInfo newInfo = new SSOInfo
+                        (UserName.getText().toString(),SSOName.getText().toString(),ISOnumber.getText().toString(),Email.getText().toString(),Address.getText().toString(),Contact.getText().toString());
+
+                myRef.child(test).setValue(newInfo);
+                /////
             }
         });
 
@@ -93,21 +103,25 @@ public class SProfileEdit extends AppCompatActivity {
         });
 
     }
-
-
+//ye kaam ho gya tha ;)
 
     //onStart will retrieve the data from realTime Database and set the value to the corresponding fields.
     @Override
     protected void onStart() {
         super.onStart();
+        //Toast.makeText(getApplicationContext(),"Ye chl rha hai",Toast.LENGTH_LONG).show();
 
-        Toast.makeText(getApplicationContext(),"Ye chl rha hai",Toast.LENGTH_LONG).show();
-
-        ValueEventListener infoListener = new ValueEventListener() {
+        Log.d("dikkat","Conatct"+myRef.child(test).child("contact"));
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                SSOInfo ssoInfo = dataSnapshot.getValue(SSOInfo.class);
+                SSOInfo ssoInfo = dataSnapshot.child(test).getValue(SSOInfo.class);
+                Log.d("dikkat1",dataSnapshot.child(test).getValue(SSOInfo.class).toString());
+                Log.d("dikkat2",dataSnapshot.child(test).getValue().toString());
+                Log.d("dikkat3",dataSnapshot.child(test).toString());
+                Log.d("dikkat4",dataSnapshot.toString());
+                //Log.d("dikkat5",dataSnapshot.child(test).getValue(SSOInfo.class).getContact());
 
                 UserName.setText(ssoInfo.getUserName());
                 SSOName.setText(ssoInfo.getSSOName());
@@ -116,15 +130,17 @@ public class SProfileEdit extends AppCompatActivity {
                 Address.setText(ssoInfo.getAddress());
                 Contact.setText(ssoInfo.getContact());
 
+                //Toast.makeText(getApplicationContext(),dataSnapshot.getValue().toString(),Toast.LENGTH_LONG).show();
+
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
+
                 Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
 
-            }};
-
-        myRef.addValueEventListener(infoListener);
+            }
+        });
 
 
     }
